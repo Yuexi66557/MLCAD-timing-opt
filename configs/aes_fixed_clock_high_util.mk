1a1ee8f7928ff3f2b489d42d0d33d66b1cd83adc
Yuexi Zhao <yzhao2497@gmail.com>
2026-05-17 17:49:18 +0800

    Add AES LLM rerun result and negative QoR case

---
+++ configs/aes_fixed_clock_high_util.mk
@@ -0,0 +1,23 @@
+export DESIGN_NICKNAME = aes
+export DESIGN_NAME = aes_cipher_top
+export PLATFORM    = nangate45
+
+export VERILOG_FILES = $(sort $(wildcard $(DESIGN_HOME)/src/$(DESIGN_NICKNAME)/*.v))
+export SDC_FILE = /mnt/d/MLCAD-timing-opt/configs/aes_fixed_clock_high_util.sdc
+
+export FLOORPLAN_DEF = $(DESIGN_HOME)/$(PLATFORM)/$(DESIGN_NICKNAME)/aes_ng45_fp.def
+
+export PLACE_DENSITY_LB_ADDON = 0.15
+export TNS_END_PERCENT = 100
+# workaround for high congestion in post-grt repair
+export SKIP_INCREMENTAL_REPAIR = 1
+
+export SWAP_ARITH_OPERATORS = 1
+export OPENROAD_HIERARCHICAL = 1
+
+
+# MLCAD Agent Generated Config
+# strategy: fixed_clock_high_util
+# source: deepseek_llm
+# mode: fixed_clock_flow_search
+# clock_period: 0.46
